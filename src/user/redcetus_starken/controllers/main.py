41e91fe5cf8c2976100fb8108c917b843848bef9
rdulon <ricardo.dulon@redcetus.cl>
2026-04-23 18:33:15 -0400

    save and preselect starken commune in checkout

--- src/user/redcetus_starken/controllers/main.py
+++ src/user/redcetus_starken/controllers/main.py
@@ -1,5 +1,27 @@
+from odoo.http import request, route
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
-if "starken_commune_id" not in WebsiteSale.WRITABLE_PARTNER_FIELDS:
-    WebsiteSale.WRITABLE_PARTNER_FIELDS.append("starken_commune_id")
+class WebsiteSaleStarken(WebsiteSale):
+
+    if "starken_commune_id" not in WebsiteSale.WRITABLE_PARTNER_FIELDS:
+        WebsiteSale.WRITABLE_PARTNER_FIELDS.append("starken_commune_id")
+
+    @route('/shop/address/submit', type='http', auth='public', methods=['POST'], website=True, sitemap=False)
+    def address_submit(self, **post):
+        response = super().address_submit(**post)
+
+        commune_id = post.get("starken_commune_id")
+        if commune_id:
+            try:
+                commune_id = int(commune_id)
+            except (TypeError, ValueError):
+                commune_id = False
+
+        if commune_id:
+            order = request.website.sale_get_order()
+            partner = order.partner_shipping_id or order.partner_id
+            if partner and partner.exists():
+                partner.sudo().write({"starken_commune_id": commune_id})
+
+        return response
