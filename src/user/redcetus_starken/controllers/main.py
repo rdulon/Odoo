b37cd53f593f9e8bdb3be7b502d39ae2f658b980
rdulon <ricardo.dulon@redcetus.cl>
2026-04-24 15:23:33 -0400

    save starken commune in shop address submit

--- src/user/redcetus_starken/controllers/main.py
+++ src/user/redcetus_starken/controllers/main.py
@@ -1,5 +1,7 @@
 import logging
 
+from odoo import http
+from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 _logger = logging.getLogger(__name__)
@@ -7,18 +9,32 @@ _logger = logging.getLogger(__name__)
 
 class WebsiteSaleStarken(WebsiteSale):
 
-    def _complete_address_values(
-        self, address_values, *args, order_sudo=False, **kwargs
+    @http.route()
+    def shop_address_submit(
+        self,
+        partner_id=None,
+        address_type='billing',
+        use_delivery_as_billing=None,
+        callback=None,
+        **form_data
     ):
-        super()._complete_address_values(
-            address_values, *args, order_sudo=order_sudo, **kwargs
+        commune_id = form_data.get("starken_commune_id")
+        _logger.warning("STARKEN SHOP ADDRESS SUBMIT COMMUNE: %s", commune_id)
+
+        response = super().shop_address_submit(
+            partner_id=partner_id,
+            address_type=address_type,
+            use_delivery_as_billing=use_delivery_as_billing,
+            callback=callback,
+            **form_data
         )
 
-        commune_id = kwargs.get("starken_commune_id")
-
-        if commune_id:
+        if commune_id and partner_id:
             try:
-                address_values["starken_commune_id"] = int(commune_id)
-                _logger.warning("STARKEN INJECTED IN ADDRESS VALUES: %s", commune_id)
-            except (TypeError, ValueError):
-                pass
+                partner = request.env["res.partner"].sudo().browse(int(partner_id))
+                partner.write({"starken_commune_id": int(commune_id)})
+                _logger.warning("STARKEN COMMUNE SAVED ON PARTNER %s", partner.id)
+            except Exception:
+                _logger.exception("STARKEN ERROR SAVING COMMUNE")
+
+        return response
