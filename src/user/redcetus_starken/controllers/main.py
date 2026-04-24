027353bd0d2c48e08e4e5f8ecce89aca0e27a394
rdulon <ricardo.dulon@redcetus.cl>
2026-04-24 11:34:54 -0400

    save starken commune through portal address controller

--- src/user/redcetus_starken/controllers/main.py
+++ src/user/redcetus_starken/controllers/main.py
@@ -2,25 +2,20 @@ import logging
 
 from odoo import http
 from odoo.http import request
-from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
 _logger = logging.getLogger(__name__)
 
 
-if "starken_commune_id" not in WebsiteSale.WRITABLE_PARTNER_FIELDS:
-    WebsiteSale.WRITABLE_PARTNER_FIELDS.append("starken_commune_id")
-
-
-class WebsiteSaleStarken(WebsiteSale):
+class StarkenCustomerPortal(CustomerPortal):
 
     @http.route()
     def address_submit(self, **post):
-        _logger.warning("STARKEN CHECKOUT POST: %s", post)
+        commune_id = post.get("starken_commune_id")
+        _logger.warning("STARKEN ADDRESS SUBMIT POST: %s", post)
+        _logger.warning("STARKEN COMMUNE RECEIVED: %s", commune_id)
 
         response = super().address_submit(**post)
-
-        commune_id = post.get("starken_commune_id")
-        _logger.warning("STARKEN COMMUNE RECEIVED: %s", commune_id)
 
         if commune_id:
             try:
@@ -30,11 +25,21 @@ class WebsiteSaleStarken(WebsiteSale):
 
         if commune_id:
             order = request.website.sale_get_order()
-            partner = order.partner_shipping_id or order.partner_id
-            _logger.warning("STARKEN PARTNER TO UPDATE: %s", partner)
+            partner = False
+
+            # En checkout, normalmente viene el partner_id en el post o en la orden.
+            partner_id = post.get("partner_id")
+            if partner_id:
+                try:
+                    partner = request.env["res.partner"].sudo().browse(int(partner_id))
+                except (TypeError, ValueError):
+                    partner = False
+
+            if not partner or not partner.exists():
+                partner = order.partner_shipping_id or order.partner_id if order else False
 
             if partner and partner.exists():
                 partner.sudo().write({"starken_commune_id": commune_id})
-                _logger.warning("STARKEN COMMUNE SAVED OK")
+                _logger.warning("STARKEN COMMUNE SAVED OK ON PARTNER %s", partner.id)
 
         return response
