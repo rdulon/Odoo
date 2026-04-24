22fc0dc8a7dcd50a8a6a2c0d6d596d0b29df3955
rdulon <ricardo.dulon@redcetus.cl>
2026-04-24 15:08:11 -0400

    inject starken commune into complete address values

--- src/user/redcetus_starken/controllers/main.py
+++ src/user/redcetus_starken/controllers/main.py
@@ -7,16 +7,18 @@ _logger = logging.getLogger(__name__)
 
 class WebsiteSaleStarken(WebsiteSale):
 
-    def _prepare_address_values(self, order_sudo, **post):
-        values = super()._prepare_address_values(order_sudo, **post)
+    def _complete_address_values(
+        self, address_values, *args, order_sudo=False, **kwargs
+    ):
+        super()._complete_address_values(
+            address_values, *args, order_sudo=order_sudo, **kwargs
+        )
 
-        commune_id = post.get("starken_commune_id")
+        commune_id = kwargs.get("starken_commune_id")
 
         if commune_id:
             try:
-                values["starken_commune_id"] = int(commune_id)
-                _logger.warning("STARKEN VALUE INJECTED: %s", values["starken_commune_id"])
+                address_values["starken_commune_id"] = int(commune_id)
+                _logger.warning("STARKEN INJECTED IN ADDRESS VALUES: %s", commune_id)
             except (TypeError, ValueError):
                 pass
-
-        return values
