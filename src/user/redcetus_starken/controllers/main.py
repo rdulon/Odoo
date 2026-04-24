fb113da5a58901e25eba9b51df04ce3dc4e53690
rdulon <ricardo.dulon@redcetus.cl>
2026-04-24 10:56:01 -0400

    debug checkout commune saving and restrict to chile

--- src/user/redcetus_starken/controllers/main.py
+++ src/user/redcetus_starken/controllers/main.py
@@ -1,6 +1,10 @@
+import logging
+
 from odoo import http
 from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+_logger = logging.getLogger(__name__)
 
 
 if "starken_commune_id" not in WebsiteSale.WRITABLE_PARTNER_FIELDS:
@@ -11,9 +15,12 @@ class WebsiteSaleStarken(WebsiteSale):
 
     @http.route()
     def address_submit(self, **post):
-        commune_id = post.get("starken_commune_id")
+        _logger.warning("STARKEN CHECKOUT POST: %s", post)
 
         response = super().address_submit(**post)
+
+        commune_id = post.get("starken_commune_id")
+        _logger.warning("STARKEN COMMUNE RECEIVED: %s", commune_id)
 
         if commune_id:
             try:
@@ -24,7 +31,10 @@ class WebsiteSaleStarken(WebsiteSale):
         if commune_id:
             order = request.website.sale_get_order()
             partner = order.partner_shipping_id or order.partner_id
+            _logger.warning("STARKEN PARTNER TO UPDATE: %s", partner)
+
             if partner and partner.exists():
                 partner.sudo().write({"starken_commune_id": commune_id})
+                _logger.warning("STARKEN COMMUNE SAVED OK")
 
         return response
